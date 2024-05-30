d0b449fb79aa14b6a5d58ffebde9d7c92141dc00
EightChromer <vinifs935@gmail.com>
2024-05-30 19:58:17 -0300

    código enviado

--- desafio-felipao-2/index.js
+++ desafio-felipao-2/index.js
@@ -1,23 +1,56 @@
-const vitorias = parseInt(gets());
-const derrotas = parseInt(gets());
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-let nivel = ""
-let saldoDeVitorias = vitorias - derrotas
+let vitorias, derrotas;
 
-if (vitorias <= 10){
-    nivel = "Ferro";
-} else if (vitorias >= 11 && vitorias <= 20 ){
-    nivel = "Bronze";
-}else if (vitorias >= 21 && vitorias <= 50 ){
-    nivel = "Prata";
-}else if (vitorias >= 51 && vitorias <= 80 ){
-    nivel = "Ouro";
-}else if (vitorias >= 81 && vitorias <= 90 ){
-    nivel = "Diamante";
-}else if (vitorias >= 91 && vitorias <= 100 ){
-    nivel = "Lendário";
-}else {
-    nivel = "Imortal";
+function lerVitorias() {
+    rl.question('Insira o número de vitórias: ', function(resposta) {
+        vitorias = parseInt(resposta);
+        
+        while (vitorias < 0 || isNaN(vitorias)) {
+            console.log('Valor inválido! O número de vitórias deve ser um número inteiro positivo.');
+            rl.prompt();
+            return lerVitorias();
+        }
+        lerDerrotas();
+    });
+}
+function lerDerrotas() {
+    rl.question('Insira o número de derrotas: ', function(resposta) {
+        derrotas = parseInt(resposta);
+        
+        while (derrotas < 0 || isNaN(derrotas)) {
+            console.log('Valor inválido! O número de derrotas deve ser um número inteiro positivo.');
+            rl.prompt();
+            return lerDerrotas();
+        }
+
+        let saldoDeVitorias = vitorias - derrotas;
+        let nivel = "";
+
+        if (vitorias <= 10) {
+            nivel = "Ferro";
+        } else if (vitorias <= 20) {
+            nivel = "Bronze";
+        } else if (vitorias <= 50) {
+            nivel = "Prata";
+        } else if (vitorias <= 80) {
+            nivel = "Ouro";
+        } else if (vitorias <= 90) {
+            nivel = "Diamante";
+        } else if (vitorias <= 100) {
+            nivel = "Lendário";
+        } else {
+            nivel = "Imortal";
+        }
+
+        console.log("O herói tem saldo de " + saldoDeVitorias + " está no nível de " + nivel);
+
+        rl.close();
+    });
 }
 
-print("O herói tem saldo de " + saldoDeVitorias + " está no nível de " + nivel)
+lerVitorias();
