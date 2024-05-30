a9e2c2946949c28556b1bf7ed676ea3b4b30bb96
EightChromer <vinifs935@gmail.com>
2024-05-30 17:41:09 -0300

    projeto adicionado

--- desafio-felipao-2/index.js
+++ desafio-felipao-2/index.js
@@ -1,1 +1,23 @@
-console.log("olá mundo!")
+const vitorias = parseInt(gets());
+const derrotas = parseInt(gets());
+
+let nivel = ""
+let saldoDeVitorias = vitorias - derrotas
+
+if (vitorias <= 10){
+    nivel = "Ferro";
+} else if (vitorias >= 11 && vitorias <= 20 ){
+    nivel = "Bronze";
+}else if (vitorias >= 11 && vitorias <= 20 ){
+    nivel = "Prata";
+}else if (vitorias >= 21 && vitorias <= 50 ){
+    nivel = "Ouro";
+}else if (vitorias >= 51 && vitorias <= 80 ){
+    nivel = "Diamante";
+}else if (vitorias >= 81 && vitorias <= 90 ){
+    nivel = "Lendário";
+}else if (vitorias >= 91 && vitorias <= 100 ){
+    nivel = "Imortal";
+}
+
+print("O herói tem saldo de " + saldoDeVitorias + " está no nível de " + nivel)
