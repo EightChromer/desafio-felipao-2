9b8502d5bdb72e4d96a2c0856679e5750740a468
EightChromer <vinifs935@gmail.com>
2024-05-30 17:44:41 -0300

    projeto adicionado

--- desafio-felipao-2/index.js
+++ desafio-felipao-2/index.js
@@ -8,15 +8,15 @@ if (vitorias <= 10){
     nivel = "Ferro";
 } else if (vitorias >= 11 && vitorias <= 20 ){
     nivel = "Bronze";
-}else if (vitorias >= 11 && vitorias <= 20 ){
-    nivel = "Prata";
 }else if (vitorias >= 21 && vitorias <= 50 ){
-    nivel = "Ouro";
+    nivel = "Prata";
 }else if (vitorias >= 51 && vitorias <= 80 ){
-    nivel = "Diamante";
+    nivel = "Ouro";
 }else if (vitorias >= 81 && vitorias <= 90 ){
-    nivel = "Lendário";
+    nivel = "Diamante";
 }else if (vitorias >= 91 && vitorias <= 100 ){
+    nivel = "Lendário";
+}else {
     nivel = "Imortal";
 }
 
